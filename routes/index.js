d799d47ab0a1bbd3fc4dc8e1c86c07bb46d85caf
MUBIRI <mubirirobberts@gmail.com>
2023-06-16 22:30:37 +0000

    Debug fix

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,20 @@
+import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
-import FilesController from '../controllers/FilesController';
-import { Express } from 'express';
+import FileController from '../controllers/FilesController';
 
-const injectRoutes = (api) => {
-    api.get('/status', AppController.getStatus);
-    api.get('/stats', AppController.getStats);
-    api.get('/connect', basicAuthenticate, AuthController.getConnect);
-    api.get('/disconnect', xTokenAuthenticate, AuthController.getDisconnect);
-    api.post('/users', UsersController.postNew);
-    api.get('/users/me', xTokenAuthenticate, UsersController.getMe);
-    api.post('/files', xTokenAuthenticate, FilesController.postUpload);
-    api.get('/files/:id', xTokenAuthenticate, FilesController.getShow);
-    api.get('/files', xTokenAuthenticate, FilesController.getIndex);
-    api.put('/files/:id/publish', xTokenAuthenticate, FilesController.putPublish);
-    api.put('/files/:id/unpublish', xTokenAuthenticate, FilesController.putUnpublish);
-    api.get('/files/:id/data', FilesController.getFile);
-  
-    api.all('*', (req, res, next) => {
-      errorResponse(new APIError(404, `Cannot ${req.method} ${req.url}`), req, res, next);
-    });
-    api.use(errorResponse);
-  };
-  
-  export default injectRoutes;
+const indexRoute = Router();
+indexRoute.get('/status', AppController.getStatus);
+indexRoute.get('/stats', AppController.getStats);
+indexRoute.post('/users', UsersController.postNew);
+indexRoute.get('/connect', AuthController.getConnect);
+indexRoute.get('/disconnect', AuthController.getDisconnect);
+indexRoute.get('/users/me', UsersController.getMe);
+indexRoute.post('/files', FileController.postUpload);
+indexRoute.get('/files/:id', FileController.getShow);
+indexRoute.get('/files', FileController.getIndex);
+indexRoute.put('/files/:id/publish', FileController.putPublish);
+indexRoute.put('/files/:id/unpublish', FileController.putUnpublish);
+indexRoute.get('/files/:id/data', FileController.getFile);
+export default indexRoute;
